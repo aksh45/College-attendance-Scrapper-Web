5896ee6ca4642de12c0855d33506ecd90a8269a6
Divneet Kaur <wordsyounevernoticed@gmail.com>
2020-07-07 15:06:47 +0530

    ssl issue

--- button/button/views.py
+++ button/button/views.py
@@ -10,24 +10,24 @@ def output(request):
     uname =  request.POST['uname']
     password = request.POST['psw']
     with requests.Session() as s:
-        r = s.get('https://guru.gndec.ac.in/login/index.php')
+        r = s.get('https://guru.gndec.ac.in/login/index.php',verify=False)
         soup = bs(r.content,features="html.parser")
         filtered = soup.find_all("input")
         payload = {'logintoken':filtered[2]['value'],'username':uname,'password':password}
-        login = s.post('https://guru.gndec.ac.in/login/index.php',data = payload)
+        login = s.post('https://guru.gndec.ac.in/login/index.php',data = payload,verify=False)
         disp_page =  bs(login.content,features="html.parser")
         drop_down = disp_page.find("ul", {"id": "dropdownmain-navigation0"})
-        subject_selected = s.get(drop_down.find('li').find('a')['href'])
+        subject_selected = s.get(drop_down.find('li').find('a')['href'],verify=False)
         subject_page = bs(subject_selected.content,features="html.parser")
         teacher_select1 = subject_page.find('ul',{"class": "section img-text"})
         teacher_select2 = teacher_select1.find('li',{'class':'activity attendance modtype_attendance'})
         attendance_link = teacher_select2.find('a',{'class':''})['href']
-        attendance = s.get(attendance_link)
+        attendance = s.get(attendance_link,verify=False)
         attendance_content = bs(attendance.content,features="html.parser")
         allcourses_search = attendance_content.find('ul',{"class": "nav nav-tabs mb-3"})
         allcourses_search1 = list(allcourses_search)[1]
         allattendance_link = allcourses_search1.find('a',{"class": "nav-link"})['href']
-        attendance_all = s.get(allattendance_link)
+        attendance_all = s.get(allattendance_link,verify=False)
         display_attendance = bs(attendance_all.content,features="html.parser")
         filtered1 = display_attendance.find_all("tr",class_ = "")
     l = []
